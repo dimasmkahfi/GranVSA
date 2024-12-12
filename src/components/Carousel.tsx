6f48af7f16f68c79d8deca10422a7d2aabcf7420
dimasmkahfi <dimasm551@gmail.com>
2024-12-12 23:44:19 +0700

    adj: Adjust Routing Registration

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,7 +6,7 @@ const CardGrid: React.FC = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        <Link href="/visa">
+        <Link href="https://registration.test/visa">
           <div className="w-full h-full flex">
             <Card
               title="Visa for Seafarer"
@@ -15,16 +15,17 @@ const CardGrid: React.FC = () => {
             />
           </div>
         </Link>
-        <Link href="/flag-license">
+        <Link href="https://registration.test/license">
           <div className="w-full h-full flex">
             <Card
               title="Flag License"
+
               description="Travel worry-free! Your tourist visa is just a click away. Fast and reliable service, and easy requirements."
               imageUrl="/img/flagli.png"
             />
           </div>
         </Link>
-        <Link href="/seafarer-documents">
+        <Link href="https://registration.test/seafarer">
           <div className="w-full h-full flex">
             <Card
               title="Seafarer Document"
--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -13,6 +13,7 @@ export const Cta = () => {
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
           <a
             href="https://wa.me/+6281385947595"
+            
             target="_blank"
             rel="noopener"
             className="inline-block py-3 mx-auto text-lg font-medium text-center text-black bg-white rounded-md px-7 lg:px-10 lg:py-5 "
